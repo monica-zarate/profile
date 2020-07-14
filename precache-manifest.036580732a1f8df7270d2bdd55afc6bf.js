0714f291659929013ad250acc0b157f20be6954d
Monica Zhazil <monicazhazil@MacBook-Pro.hitronhub.home>
2020-07-14 15:52:20 -0700

    Updates

---
+++ precache-manifest.036580732a1f8df7270d2bdd55afc6bf.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "922627c62d7b36fa9629ad62fcdde946",
+    "url": "/profile/index.html"
+  },
+  {
+    "revision": "7ae8d061b0a96cbf1e90",
+    "url": "/profile/static/css/main.b026878e.chunk.css"
+  },
+  {
+    "revision": "8ed288b81aa316813cdf",
+    "url": "/profile/static/js/2.7d9cd82b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/profile/static/js/2.7d9cd82b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7ae8d061b0a96cbf1e90",
+    "url": "/profile/static/js/main.cb1b6cfc.chunk.js"
+  },
+  {
+    "revision": "5076af70b647b2393d8e",
+    "url": "/profile/static/js/runtime-main.8771bb24.js"
+  },
+  {
+    "revision": "69ed615006476f4a1ae3c770a0a88ac6",
+    "url": "/profile/static/media/app.69ed6150.png"
+  },
+  {
+    "revision": "97ec4fb637c611ebf970777f704e9943",
+    "url": "/profile/static/media/edu.97ec4fb6.svg"
+  },
+  {
+    "revision": "ddc250127189a3b4ee4802e6c2493757",
+    "url": "/profile/static/media/github.ddc25012.svg"
+  },
+  {
+    "revision": "f41e3529da2eaff601fcfad41471162f",
+    "url": "/profile/static/media/laptop.f41e3529.svg"
+  },
+  {
+    "revision": "6c8e140e0c8b5d6acd513c67d12d267d",
+    "url": "/profile/static/media/linkedin.6c8e140e.svg"
+  },
+  {
+    "revision": "e6956db22f6b55500046d41bb128484a",
+    "url": "/profile/static/media/mail.e6956db2.svg"
+  },
+  {
+    "revision": "58c1d842950c9aae78818c208b9d9be7",
+    "url": "/profile/static/media/more.58c1d842.svg"
+  },
+  {
+    "revision": "54262e539a23ed12839a1a75cfa3f0cb",
+    "url": "/profile/static/media/mz2.54262e53.jpg"
+  },
+  {
+    "revision": "e98f4e1b3fe28f91034dc8e3048cfea9",
+    "url": "/profile/static/media/pdf.e98f4e1b.svg"
+  },
+  {
+    "revision": "e07df86cef2e721115583d61d1fb68a6",
+    "url": "/profile/static/media/roboto-bold.e07df86c.ttf"
+  },
+  {
+    "revision": "58aef543c97bbaf6a9896e8484456d98",
+    "url": "/profile/static/media/roboto-medium.58aef543.ttf"
+  },
+  {
+    "revision": "11eabca2251325cfc5589c9c6fb57b46",
+    "url": "/profile/static/media/roboto-regular.11eabca2.ttf"
+  },
+  {
+    "revision": "5b025a51d20fec85948f10d955078897",
+    "url": "/profile/static/media/robotoslab-bold.5b025a51.ttf"
+  },
+  {
+    "revision": "f2add96fdf83aa64a9792252dc34c589",
+    "url": "/profile/static/media/robotoslab-medium.f2add96f.ttf"
+  },
+  {
+    "revision": "f7ffac600a7a4edb6db9b21c606a6884",
+    "url": "/profile/static/media/robotoslab-regular.f7ffac60.ttf"
+  },
+  {
+    "revision": "7e145c9eb60da95379f070be9920eca5",
+    "url": "/profile/static/media/twitter.7e145c9e.svg"
+  }
+]);

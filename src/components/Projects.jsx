0bf95574ff464e8ad687810fe9625bcc8a47544e
Monica Zhazil <monicazhazil@MacBook-Pro.hitronhub.home>
2020-07-15 18:04:08 -0700

    Styled for mobile device.

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -77,8 +77,6 @@ function Projects() {
       </button>
       <iframe
         className="projects__video"
-        width="640"
-        height="353"
         src="https://www.loom.com/embed/362935b8214541e2aed0368b2427cd73"
         frameborder="0"
         webkitallowfullscreen

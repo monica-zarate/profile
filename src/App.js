827900e46f6fce41f469fae22152ecac07346371
Monica Zhazil <monicazhazil@MacBook-Pro.hitronhub.home>
2020-09-16 16:30:41 -0700

    Added a Route for About and Projects Components. Added a Header and styled it for mobile.

--- src/App.js
+++ src/App.js
@@ -1,16 +1,21 @@
 import React from "react";
 import "./components/styles/main.css";
+import { Route, Switch } from "react-router-dom";
+import Landing from "./components/Landing";
+import Header from "./components/Header";
 import About from "./components/About";
-
 import Projects from "./components/Projects";
 
 class App extends React.Component {
   render() {
     return (
       <div className="App">
-        <About />
-
-        <Projects />
+        <Header />
+        <Switch>
+          <Route path="/" component={Landing} />
+          <Route path="/about" component={About} />
+          <Route path="/projects" component={Projects} />
+        </Switch>
       </div>
     );
   }
---
+++ src/components/Header.jsx
@@ -0,0 +1,56 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import "./styles/main.css";
+
+function Header() {
+  let navSlide = () => {
+    const burger = document.querySelector(".header__burger");
+    const nav = document.querySelector(".header__nav");
+    const navLinks = document.querySelectorAll(".header__li");
+    console.log("toggling");
+
+    // Toggle Nav
+    nav.classList.toggle("active");
+
+    // Animate Links
+    navLinks.forEach((link, index) => {
+      if (link.style.animation) {
+        link.style.animation = "";
+      } else {
+        link.style.animation = `nav-fade 0.5s ease forwards ${
+          index / 7 + 1.5
+        }s`;
+      }
+    });
+
+    // Burger Animation
+    burger.classList.toggle("toggle");
+  };
+
+  return (
+    <div className="header">
+      <Link to={`/`} className="header__title-link">
+        <h1 className="header__title">MZ</h1>
+      </Link>
+      <ul className="header__nav">
+        <li className="header__li">
+          <Link to={`/about`} className="header__link">
+            About
+          </Link>
+        </li>
+        <li className="header__li">
+          <Link to={`/projects`} className="header__link">
+            Projects
+          </Link>
+        </li>
+      </ul>
+      <div className="header__burger" onClick={navSlide}>
+        <div className="line1"></div>
+        <div className="line2"></div>
+        <div className="line3"></div>
+      </div>
+    </div>
+  );
+}
+
+export default Header;
